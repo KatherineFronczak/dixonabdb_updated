4f1b8b64d1cd21d31a3815718c3bac85e6656586
KatherineFronczak <katherinefronczak@gmail.com>
2022-04-02 07:58:31 -0400

    initial commit

--- abdb/views.py
+++ abdb/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
+def database(request):
     return render(request, 'abdb/database.html')
